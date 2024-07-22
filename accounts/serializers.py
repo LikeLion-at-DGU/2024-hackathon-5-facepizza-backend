52e7f704453616880bda068b8fb5a29e5df77c5b
ryann1203 <oxford1203@naver.com>
2024-07-23 00:48:19 +0900

    feat: 로그인, 회원가입 구현

---
+++ accounts/serializers.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
+
+from django.contrib.auth import authenticate
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required = True,
+        validators = [UniqueValidator(queryset=User.objects.all())],
+    )
+
+    password = serializers.CharField(
+        write_only = True,
+        required = True,
+        validators = [validate_password],
+    )
+
+    password2 = serializers.CharField( #비밀번호 확인을 위한 필드
+        write_only = True,
+        required = True,
+    )
+
+    class Meta:
+        model = User
+        #username = ID, first_name = 사용자 이름, password2 = 비밀번호 검증용
+        fields = ('username', 'first_name', 'email', 'password', 'password2')
+
+    def validate(self, data): #password와 password2의 일치 여부 확인
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError(
+                {"password" : "비밀번호가 일치하지 않습니다."}
+            )
+        return data
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            first_name = validated_data['first_name'],
+            email = validated_data['email'],
+        )
+        
+        # 비밀번호 해싱하여 저장
+        user.set_password(validated_data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return user
+    
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only = True)
+    #write_only = True : 클라이언트 -> 서버의 역직렬화는 가능하지만 서버 -> 클라이언트는 불가능
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)
+            return token
+        
+        raise serializers.ValidationError( # 가입된 유저가 없을 경우
+            {
+                "error" : "잘못된 정보입니다."
+            }
+        )
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from .views import RegisterView, LoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
