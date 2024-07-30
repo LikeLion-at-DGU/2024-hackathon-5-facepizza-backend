e57a5c5c2a77a56eeae0449c3153aa5a721310d1
ryann1203 <oxford1203@naver.com>
2024-07-30 16:24:25 +0900

    Fix: 회원가입 시 아이디 제외

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -27,7 +27,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         #username = ID, first_name = 사용자 이름, password2 = 비밀번호 검증용
-        fields = ('username', 'first_name', 'email', 'password', 'password2')
+        fields = ('first_name', 'email', 'password', 'password2')
 
     def validate(self, data): #password와 password2의 일치 여부 확인
         if data['password'] != data['password2']:
@@ -38,7 +38,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         user = User.objects.create_user(
-            username = validated_data['username'],
+            username = validated_data['email'],
             first_name = validated_data['first_name'],
             email = validated_data['email'],
         )
@@ -51,11 +51,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only = True)
     #write_only = True : 클라이언트 -> 서버의 역직렬화는 가능하지만 서버 -> 클라이언트는 불가능
     def validate(self, data):
-        user = authenticate(**data)
+        user = authenticate(username=data['email'], password=data['password'])
         if user:
             # 기존 토큰 삭제
             Token.objects.filter(user=user).delete()
