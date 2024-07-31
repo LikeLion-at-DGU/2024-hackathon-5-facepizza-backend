e6bd1b9c3f08cb853e3e4e2fa11cb35279e22357
jiyoon607 <jiyoon607@naver.com>
2024-07-31 18:31:57 +0900

    Fix: Achievement모델 삭제, Mission모델 생성

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import MypageViewSet, AchievementViewSet
+from .views import MypageViewSet
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,10 +10,6 @@ app_name = 'mypage'
 default_router = routers.SimpleRouter(trailing_slash=False)
 default_router.register("mypage", MypageViewSet, basename="mypage")
 
-achievement_router = routers.SimpleRouter(trailing_slash=False)
-achievement_router.register("achievements", AchievementViewSet, basename="achievements")
-
 urlpatterns = [
-    path('', include(default_router.urls)),
-    path('', include(achievement_router.urls)),
+    path('', include(default_router.urls))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
