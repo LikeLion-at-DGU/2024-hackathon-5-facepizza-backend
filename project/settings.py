f26b20525dcd1042b26fca58a82ea033af72f066
ryann1203 <oxford1203@naver.com>
2024-08-03 16:10:01 +0900

    Feat: mypage 퍼센트 구현중

--- project/settings.py
+++ project/settings.py
@@ -123,6 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
