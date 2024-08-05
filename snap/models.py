4cacc854f1e3bc378b59d8d17f60064c9cfa4a3e
jiyoon607 <jiyoon607@naver.com>
2024-08-06 05:20:10 +0900

    Fix: snap 이미지필드 수정

--- snap/models.py
+++ snap/models.py
@@ -7,12 +7,12 @@ from django.dispatch import receiver
 
 # Create your models here.
 
-EMOTION_CHOICES = [
-    ('행복', 'happy'),
-    ('슬픔', 'sad'),
-    ('분노', 'angry'),
-    ('놀람', 'surprised')
-]
+EMOTION_MAP = {
+    '행복': 'happy',
+    '슬픔': 'sad',
+    '분노': 'angry',
+    '놀람': 'surprised'
+}
 
 def image_upload_path(instance, filename):
     return f'{instance.user.username}/{filename}'
@@ -20,7 +20,13 @@ def image_upload_path(instance, filename):
 class EmotionImage(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
-    emotion = models.CharField(max_length=50, choices=EMOTION_CHOICES)
+    image = models.TextField(blank=True)
+    #image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
+    emotion = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.emotion in EMOTION_MAP:
+            self.emotion = EMOTION_MAP[self.emotion]
+        super().save(*args, **kwargs)
